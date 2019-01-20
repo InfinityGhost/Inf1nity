1392d05dc5dce9792c6942ef3420a05914ab3acf
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2019-01-20 14:20:19 -0500

    Change names, add SafeConverter

--- Inf1nity/Tools/ReadWrite.cs
+++ Inf1nity/Tools/ReadWrite.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Inf1nity.FileHelper
+namespace Inf1nity.Tools
 {
-    public static class ReadWrite
+    public static class ReadWriteHelper
     {
         public static string Fetch(this IEnumerable<string> content, string prefix, string splitter = ":")
         {
---
+++ Inf1nity/Tools/SafeConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inf1nity.Tools
+{
+    public static class SafeConverter
+    {
+        public static string String(this object obj, string fallback = "")
+        {
+            try
+            {
+                return obj.ToString();
+            }
+            catch
+            {
+                return fallback;
+            }
+        }
+
+        public static int Int32(this object obj, int fallback = 0)
+        {
+            try
+            {
+                return Convert.ToInt32(obj);
+            }
+            catch
+            {
+                return fallback;
+            }
+        }
+
+        public static bool Boolean(this object obj, bool fallback = false)
+        {
+            try
+            {
+                return Convert.ToBoolean(obj);
+            }
+            catch
+            {
+                return fallback;
+            }
+        }
+    }
+}
