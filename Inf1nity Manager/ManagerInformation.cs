15743f6b97c89e578af8b7528f44f6401663ee1d
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2019-03-16 12:01:31 -0400

    Add ui version to aboutbox

---
+++ Inf1nity Manager/ManagerInformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inf1nity_Manager
+{
+    public class ManagerInformation
+    {
+        /// <summary>
+        /// Version of the program.
+        /// </summary>
+        public static string AssemblyVersion => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+    }
+}
