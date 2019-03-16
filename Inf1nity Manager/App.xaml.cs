1ece26c1292a3ddcfcc3befd81eacc62ea46a3ed
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2019-03-16 14:35:53 -0400

    Add UnhandledExceptionHandler
    Logs crashes

--- Inf1nity Manager/App.xaml.cs
+++ Inf1nity Manager/App.xaml.cs
@@ -12,6 +12,30 @@ namespace Inf1nity_Manager
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
-    {
+    {   
+        public App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += GlobalUnhandledException;
+        }
+
+        private void GlobalUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = default;
+            ex = (Exception)e.ExceptionObject;
+
+            var crashDump = new List<string>
+            {
+                $"-------",
+                $"Exception occured at {DateTime.Now}",
+                $"Source: {ex.Source}",
+                $"Message: {ex.Message}",
+                $"HelpLink: {ex.HelpLink}",
+                $"StackTrace: {Environment.NewLine}{ex.StackTrace}",
+                $"TargetSite: {ex.TargetSite.Name}",
+                $"HResult: {ex.HResult}",
+            };
+
+            System.IO.File.AppendAllLines(System.IO.Directory.GetCurrentDirectory() + "\\dump" + ".log", crashDump);
+        }
     }
 }
