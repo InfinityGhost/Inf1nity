52e67e4e553ff2b0c0a43713abb0a083ca5b1581
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2019-03-16 03:56:33 -0400

    Add animations

--- Inf1nity Manager/Controls/Items/DiscordMessage.xaml.cs
+++ Inf1nity Manager/Controls/Items/DiscordMessage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -102,6 +103,12 @@ namespace Inf1nity_Manager.Controls.Items
         #region Tools
 
 
+
+        #endregion
+
+        #region Animation
+
+        
 
         #endregion
 
