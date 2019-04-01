abc2e6653bed2eab00836689a402db1fb43704ce
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2019-04-01 00:31:57 -0400

    Add permissions

--- Inf1nity Manager/Browse/DiscordChannel.xaml.cs
+++ Inf1nity Manager/Browse/DiscordChannel.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -30,6 +31,54 @@ namespace Inf1nity_Manager.Browse
                 var msgs = await Channel.GetMessagesAsync().FlattenAsync();
                 foreach (var msg in msgs.Reverse())
                     await MessagePanel.AddMessage(msg).ConfigureAwait(false);
+
+                var client = (Application.Current.MainWindow as MainWindow).Bot.Client;
+                var guildUser = Channel.Guild.Users.First(e => e.Id == client.CurrentUser.Id);
+                var guildPerms = guildUser.GuildPermissions;
+                var channelUserPerms = Channel.GetPermissionOverwrite(client.CurrentUser);
+
+                if (Channel.PermissionOverwrites.All(e => e.Permissions.SendMessages == PermValue.Allow))
+                {
+                    Input.IsEnabled = true;
+                    return;
+                }
+
+                var rolesPerms = Channel.PermissionOverwrites.Where(r => r.TargetType == PermissionTarget.Role);
+                var botRoles = guildUser.Roles;
+
+                bool? roleCanSendMessage = null;
+                bool userCanSendMessage = false;
+
+                foreach (var r in botRoles)
+                {
+                    var ow = Channel.GetPermissionOverwrite(r);
+                    if (ow is OverwritePermissions perm)
+                    {
+                        if (perm.SendMessages == PermValue.Allow)
+                        {
+                            roleCanSendMessage = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (roleCanSendMessage == false)
+                {
+                    if (channelUserPerms.HasValue && channelUserPerms.Value.SendMessages.HasFlag(PermValue.Allow))
+                        userCanSendMessage = true;
+                    else if (channelUserPerms.HasValue && channelUserPerms.Value.SendMessages.HasFlag(PermValue.Inherit) && guildPerms.SendMessages)
+                        userCanSendMessage = true;
+                    else if (!channelUserPerms.HasValue && roleCanSendMessage.HasValue)
+                        userCanSendMessage = roleCanSendMessage.Value;
+                    else if (!channelUserPerms.HasValue && !roleCanSendMessage.HasValue)
+                        userCanSendMessage = false;
+                    else
+                        userCanSendMessage = false;
+                }
+                else if (roleCanSendMessage.HasValue)
+                    userCanSendMessage = roleCanSendMessage.Value;
+
+                Input.IsEnabled = userCanSendMessage;
             }
             catch(Exception ex)
             {
@@ -40,6 +89,8 @@ namespace Inf1nity_Manager.Browse
                     VerticalAlignment = VerticalAlignment.Center,
                 };
                 MessagePanel.Background = (Brush)new BrushConverter().ConvertFromString("#FFF0F0");
+
+                Input.IsEnabled = false;
             }
         }
 
